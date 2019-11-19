8e89ea4e53be386f830109dbede3b0427b412016
dminikm <dminik.m@email.cz>
2019-11-19 13:53:46 +0100

    Third attempt at faster color blening

--- src/app/ColorBuffer.java
+++ src/app/ColorBuffer.java
@@ -6,19 +6,10 @@ import javafx.scene.image.PixelFormat;
 class ARGBColor {
     public static int mixARGBIntColors(int color1, int color2) {
         int alpha = (color2 & 0xFF000000) >>> 24;
-
-        int rb = (
-            (( color2 & 0xFF00FF ) * alpha) +
-            (( color1 & 0xFF00FF) * (0xFF - alpha))
-        ) & 0xFF00FF00;
-
-        int g = (
-            ((color2 & 0xFF00) * alpha) + 
-            ((color1 & 0xFF00) * (0xFF - alpha))
-        ) & 0x00FF0000;
-
-        int a = 0xFF000000;
-        return a | (rb | g) >>> 8;
+        int rb = (color1 & 0xFF00FF) + (((color2 & 0xFF00FF) - (color1 & 0xFF00FF)) * alpha >>> 8);
+        int g = (color1 & 0x00FF00) + (((color2 & 0x00FF00) - (color1 & 0x00FF00)) * alpha >>> 8);
+        
+        return 0xFF000000 | (rb & 0xFF00FF) | (g & 0xFF00);
     }
 }
 
